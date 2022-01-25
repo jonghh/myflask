de4f789ac44b99ada9926001a2e61f673c809923
82104 <jonghhhhhh@gmail.com>
2022-01-25 12:05:26 +0900

    1st commit

--- main/__init__.py
+++ main/__init__.py
@@ -1,3 +1,7 @@
+#EC2 flask 관련 사항:
+#https://ndb796.tistory.com/244
+#https://medium.com/analytics-vidhya/deploy-a-flask-app-on-amazon-aws-ec2-and-keep-it-running-while-you-are-offline-38d22571e2c5
+
 from flask import Flask
 from .view import hello
 
